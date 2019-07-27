a4efd8ec4a71102487abc8f9386ac4e774cd2788
Idan Loo <im@siwei.lu>
2019-07-28 01:59:35 +0800

    fix(start): avoid modifing the dist folder

--- scripts/start.js
+++ scripts/start.js
@@ -1,12 +1,13 @@
 const Parcel = require('parcel-bundler')
 const { resolve } = require('path')
 
-const cacheDir = resolve(__dirname, '../.cache')
-const entrypoint = resolve(process.cwd(), './src/index.html')
-const port = process.env.SERVE_PORT || 1234
+const cacheDir = resolve(__dirname, '..', '.cache')
+const entrypoint = resolve(process.cwd(), '.', 'src', 'index.html')
+const outDir = resolve(__dirname, '..', '.tmp')
+const port = process.env.PORT || 1234
 
 function start() {
-  new Parcel(entrypoint, { cacheDir }).serve(port)
+  new Parcel(entrypoint, { cacheDir, outDir }).serve(port)
 }
 
 module.exports = start
