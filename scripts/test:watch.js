6e4e0b3c1ac0802f72942c48b48b42ed0c0d6aa6
Idan Loo <im@siwei.lu>
2019-10-19 22:43:22 +0800

    chore: remove the template fold from git

--- scripts/test:watch.js
+++ scripts/test:watch.js
@@ -3,7 +3,7 @@ const { resolve } = require('path')
 const config = resolve(__dirname, '../jest.config.js')
 
 function test() {
-  execSync(`jest -c ${config}`, { stdio: 'inherit' })
+  execSync(`jest --watch -c ${config}`, { stdio: 'inherit' })
 }
 
 module.exports = test
--- template/src/controls/Like/stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { action } from '@storybook/addon-actions'
-import { storiesOf } from '@storybook/react'
-import Like from '.'
-
-storiesOf('Like', module)
-  .add('default', () => <Like count={10} />)
-  .add('withAction', () => <Like count={10} onClick={action('click')} />)
