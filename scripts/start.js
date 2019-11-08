6705f5e3f59f06676951ecb03063b0c146949352
Idan Loo <im@siwei.lu>
2019-11-08 23:58:25 +0800

    feat: proxy to remote

--- scripts/start.js
+++ scripts/start.js
@@ -1,13 +1,26 @@
 const Parcel = require('parcel-bundler')
+const express = require('express')
+const proxy = require('http-proxy-middleware')
 const { resolve } = require('path')
 
+const cwd = process.cwd()
 const cacheDir = resolve(__dirname, '..', '.cache')
-const entrypoint = resolve(process.cwd(), '.', 'src', 'index.html')
+const entrypoint = resolve(cwd, '.', 'src', 'index.html')
 const outDir = resolve(__dirname, '..', '.tmp')
 const port = process.env.PORT || 1234
+const pkg = require(resolve(cwd, 'package.json'))
+
+const app = express()
+
+const proxyConfig = pkg.proxy
+if (proxyConfig) {
+  Object.entries([k, v] => app.use(k, proxy(v, { changeOrigin: true })))
+}
+
 
 function start() {
-  new Parcel(entrypoint, { cacheDir, outDir }).serve(port)
+  const bundler = new Parcel(entrypoint, { cacheDir, outDir })
+  app.use(bundler.middleware).listen(port)
 }
 
 module.exports = start
